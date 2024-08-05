4a97fcefa12e87b8135f881bfc922e7490065f93
Jamie <Jamie.witcher@yahoo.co.uk>
2024-08-05 13:00:34 +0100

    this commit saves the video

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+from utils import read_video, save_video
+
 def main():
-    print("Hello World")
+    # Read Video
+    video_frames = read_video(r"C:\Users\44780\Documents\python_motion_tracking_again\input_videos\08fd33_4.mp4")
+
+    # Save video
+    save_video(video_frames, r"C:\Users\44780\Documents\python_motion_tracking_again\output\output_video.avi")
 
 if __name__ == "__main__":
     main()
