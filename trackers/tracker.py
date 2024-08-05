2e0895fca362960b615318fce404672111bdf2e0
Jamie <Jamie.witcher@yahoo.co.uk>
2024-08-05 16:57:21 +0100

    this commit prints supervision format

--- trackers/tracker.py
+++ trackers/tracker.py
@@ -12,10 +12,18 @@ class Tracker:
         for i in range(0,len(frames),batch_size):
             detections_batch = self.model.predict(frames[i:i+batch_size],conf=0.1)
             detections += detections_batch
+            break
         return detections
 
     def get_object_tracks(self, frames):
-
-
-
+        
         detections = self.detect_frames(frames)
+
+        for frame_num, detection in enumerate(detections):
+            cls_names = detection.names
+            cls_names_inv = {v:k for k,v in cls_names.items()}
+
+            # Covert to supervision Detection format
+            detection_supervision = sv.Detections.from_ultralytics(detection)
+
+            print(detection_supervision)
