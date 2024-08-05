461e6e2d74647f402430f7b872c8e73194adcfde
Jamie <Jamie.witcher@yahoo.co.uk>
2024-08-05 20:20:53 +0100

    this commit adds a tracker id

--- trackers/tracker.py
+++ trackers/tracker.py
@@ -31,6 +31,7 @@ class Tracker:
                 if cls_names[class_id] == "goalkeeper":
                     detection_supervision.class_id[object_ind] = cls_names_inv["player"]
 
-            print(detection_supervision)
-
-            break
+            # Track Objects
+            detection_with_tracks = self.tracker.update_with_detections(detection_supervision)
+            
+            print(detection_with_tracks)
