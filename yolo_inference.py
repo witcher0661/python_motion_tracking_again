e831ab98b78a5bd6a62d4e6acb57f44d40fbce21
Jamie <Jamie.witcher@yahoo.co.uk>
2024-08-05 10:19:04 +0100

    this commit runs yolo inference

---
+++ yolo_inference.py
@@ -0,0 +1,9 @@
+from ultralytics import YOLO 
+
+model = YOLO("yolov8x")
+
+results = model.predict(r"C:\Users\44780\Documents\python_motion_tracking_again\input_videos\08fd33_4.mp4",save=True)
+print(results[0])
+print('=====================================')
+for box in results[0].boxes:
+    print(box)
