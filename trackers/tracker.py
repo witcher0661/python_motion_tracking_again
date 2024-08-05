f4b555dbea61371d5e16b9d83fef3debd3779ba3
Jamie <Jamie.witcher@yahoo.co.uk>
2024-08-05 20:54:05 +0100

    this commit saves the tracker as a pickle file

--- trackers/tracker.py
+++ trackers/tracker.py
@@ -1,5 +1,7 @@
 from ultralytics import YOLO
 import supervision as sv
+import pickle
+import os
 
 class Tracker:
     def __init__(self, model_path):
@@ -12,12 +14,16 @@ class Tracker:
         for i in range(0,len(frames),batch_size):
             detections_batch = self.model.predict(frames[i:i+batch_size],conf=0.1)
             detections += detections_batch
-            break
         return detections
 
-    def get_object_tracks(self, frames):
+    def get_object_tracks(self, frames, read_from_stub=False, stub_path=None):
         
         detections = self.detect_frames(frames)
+
+        if read_from_stub and stub_path is not None and os.path.exists(stub_path):
+            with open(stub_path,'rb') as f:
+                tracks = pickle.load(f)
+            return tracks
 
         tracks={
             "players":[],
@@ -61,5 +67,9 @@ class Tracker:
 
                 if cls_id == cls_names_inv['ball']:
                     tracks["ball"][frame_num][1] = {"bbox":bbox}
+
+        if stub_path is not None:
+            with open(stub_path,'wb') as f:
+                pickle.dump(tracks,f)
             
-            print(detection_with_tracks)
+        return tracks
